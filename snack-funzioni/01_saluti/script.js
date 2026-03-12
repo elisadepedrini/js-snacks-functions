f2ee21af28c39e600707b83394ac19bed9e7ef75
elisadepedrini <elisa.depedrini@gmail.com>
2026-03-12 18:28:45 +0100

    Complete task 1

--- snack-funzioni/01_saluti/script.js
+++ snack-funzioni/01_saluti/script.js
@@ -1,13 +1,22 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const userName = 'Mario';
-
+const userName = 'Mario'; 
 
 // Dichiara la funzione qui.
+function hello(name) {
+    return 'ciao ' + name
+}
+
+const saluto = (name) => {
+    return 'ciao ' + name
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(hello(userName))
+
+console.log(saluto(userName))
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
