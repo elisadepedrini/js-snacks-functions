d5b3540ce871a113b87071e7d5dbd3d1a51e8a9e
elisadepedrini <elisa.depedrini@gmail.com>
2026-03-17 17:27:14 +0100

    Complete task 2

--- snack-funzioni/02_iniziali/script.js
+++ snack-funzioni/02_iniziali/script.js
@@ -5,9 +5,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function initialLetter (arr) {
+    const initials = []
+
+    for (let i = 0; i < arr.length; i++) {
+        initials.push(arr[i][0])
+    }
+
+    return initials
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(initialLetter(names))
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
